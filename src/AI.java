65929ca4fa5f02052653d86c9a5f2ea2bfe57acd
Seth Vizel <sethv15@nycstudents.net>
2023-01-18 13:19:18 -0500

    AHHHHHHHHHHHH

--- src/AI.java
+++ src/AI.java
@@ -3,44 +3,46 @@ import java.util.Scanner;
 public class AI {
 
     public static void placeBestMove(Board board, Player[] players, int player){
-        Point currentMove = null;
+        Point bestMove = null;
+        int best = -100;
+
         for (int y = 0; y < board.getSize(); y++) {
             for (int x = 0; x < board.getSize(); x++) {
                 if (board.recordMove(x,y,players[player])) {
-                    currentMove = new Point(x,y);
-                    int winner = minimax(board, players, (player + 1) % players.length);
-                    if (winner == player || winner == -1) {
-                        return;
-                    } else {
-                        board.undoMove(x,y);
+                    int score = minimax(board, players, (player + 1) % players.length, 0);
+                    if (betterScore(score, best, player) == score) {
+                        best = score;
+                        bestMove = new Point(x,y);
                     }
+                    board.undoMove(x,y);
                 }
             }
         }
-        board.recordMove(currentMove.x(), currentMove.y(), players[player]);
+        board.recordMove(bestMove.x(), bestMove.y(), players[player]);
     }
 
-    public static int minimax(Board board, Player[] players, int turn) {
+    private static int minimax(Board board, Player[] players, int turn, int depth) {
         int winner = WinCondition.checkAll(board);
 
         if (board.isFull() || winner != -1) {
             return winner;
         }
 
+        int best = 100;
+
         for (int y = 0; y < board.getSize(); y++) {
             for (int x = 0; x < board.getSize(); x++) {
                 if (!board.recordMove(x,y,players[turn])) {
                     continue;
                 }
-                /*board.drawBoard();
-                (new Scanner(System.in)).nextLine();*/
-                winner = minimax(board, players, (turn + 1) % players.length);
+                best = betterScore(best, minimax(board, players, (turn + 1) % players.length, depth + 1) - turn, turn);
                 board.undoMove(x,y);
-                if (winner == turn || winner == -1) {
-                    return turn;
-                }
             }
         }
-        return winner;
+        return best;
+    }
+
+    private static int betterScore(int score1, int score2, int playerNum) {
+        return Math.abs(playerNum - score1) < Math.abs(playerNum - score2) ? score1 : score2;
     }
 }
